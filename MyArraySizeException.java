fd5cdd6df796b02a8e44603fbe5aaee2011f0e26
KolesnichenkoAnastasiya <79326207+KolesnichenkoAnastasiya@users.noreply.github.com>
2021-07-16 13:08:13 +0300

    Add files via upload

---
+++ MyArraySizeException.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class MyArraySizeException extends RuntimeException {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+    public MyArraySizeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
