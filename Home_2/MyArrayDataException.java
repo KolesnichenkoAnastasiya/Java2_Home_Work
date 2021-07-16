f207c824ca447311d049e058729954c9ed58bdc3
KolesnichenkoAnastasiya <22028@mail.ru>
2021-07-16 14:05:01 +0300

    done home work 2

---
+++ Home_2/MyArrayDataException.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class MyArrayDataException extends RuntimeException {
+    public MyArrayDataException (String message) {
+        super(message);
+    }
+    public MyArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
